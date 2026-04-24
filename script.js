1f630ae87cd2d95b2b466e7cc6e2f1d793c82f40
Pranav Pradeep <pranavpradeep@Pranavs-MacBook-Air.local>
2026-04-24 01:25:53 +0100

    Portfolio

--- script.js
+++
@@ -1,8 +0,0 @@
-function openModal(src) {
-    document.getElementById("certModal").style.display = "block";
-    document.getElementById("modalImg").src = src;
-}
-
-function closeModal() {
-    document.getElementById("certModal").style.display = "none";
-}
